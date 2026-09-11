d8945b5d04f020702beecb988ddb2ce0e3fb8ca8
aleontiev <alonetiev@gmail.com>
2026-09-10 22:00:37 -0500

    api type password default for password fields

--- dynamic_rest/fields/password.py
+++ dynamic_rest/fields/password.py
@@ -6,4 +6,12 @@ class DynamicPasswordField(
     CharField,
     DynamicField
 ):
-    pass
+    """A string field whose value is a secret.
+
+    Reported to clients with the "password" API type so that UIs can
+    mask the value and offer a reveal control.
+    """
+
+    def __init__(self, *args, **kwargs):
+        kwargs.setdefault('api_type', 'password')
+        super(DynamicPasswordField, self).__init__(*args, **kwargs)
